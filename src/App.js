895eb27ac64faa3a9f643cf64d53bb65dda588f5
Abdul Karim Yussif <usivabdulkarim@gmail.com>
2024-06-05 17:11:46 +0000

    update our menu-items to navigate useNavigate hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import  React from 'react';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 
-function App(){
-  return <div> 
-   <HomePage/>
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
   </div>
+);
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/hats" element={<HatsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,27 +1,29 @@
-import React from "react";
-
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    
-    <div className= {`${size} menu-item`}> 
-      
-    <div className="background-image" 
-        style={{backgroundImage:`url(${imageUrl})`}}
-    />
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    <div className='content'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <span className='subtitle'>SHOP NOW</span>
-     </div> 
-       </div>
-);
+  const handleClick = () => {
+    navigate(`${location.pathname}${linkUrl}`);
+  };
 
+  return (
+    <div className={`${size} menu-item`} onClick={handleClick}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-
-
- export default MenuItem;
+export default MenuItem;
